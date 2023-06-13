71d5cb4f1c22aa07e236e52d416ef8088bc5ce47
“arrowxgoutam” <goutampaik42@gmail.com>
2023-06-13 23:21:02 +0600

    monule-2 livetest

--- Module-2/assignment.js
+++ Module-2/assignment.js
@@ -4,3 +4,9 @@ for (i=0; i < x.length; i++) {
      sum += x[i]
 }
 console.log("The sum Result is: "+sum);
+
+
+var hello = "Hello JavaScript"
+for (i = 0; i < 100; i++) {
+     console.log(hello);
+}
